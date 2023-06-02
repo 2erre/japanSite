a247beb8c9beb8c07b8818c2038051910368f83a
Ruben <rubenrizzo15@gmail.com>
2023-06-02 19:34:24 +0200

    Sistemato html, css e js del nav-bar

--- siteScripts.js
+++ siteScripts.js
@@ -1,5 +1,13 @@
+var clickCounter = 0;
 function showNav() {
-    /*var nav = document.getElementsByClassName("nav");
-
-    nav.classList.add("nav-after")*/
+    var nav = document.getElementById("nav");
+    
+    if(clickCounter%2 != 0) {
+        nav.classList.remove("nav-after");
+        nav.classList.add("none");     
+    } else {
+        nav.classList.remove("none");    
+        nav.classList.add("nav-after");  
+    }   
+    clickCounter++;
 }
